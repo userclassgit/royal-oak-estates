aa6505b3932a52dd44fcf8908d849a960d89705b
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-03-13 15:12:41 -0500

    x button functional added

--- assets/script/detail.js
+++ assets/script/detail.js
@@ -37,7 +37,7 @@ homes.forEach((home, index) => {
 //Login dialog interaction
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("#login-btn");
-
+const xButton = document.querySelector('.x-mark');
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
   dialog.showModal();
@@ -49,3 +49,5 @@ dialog.addEventListener('click', (event) => {
     dialog.close();
   }
 });
+//closes with x button
+xButton.addEventListener('click', () => dialog.close() );
