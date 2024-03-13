bc26ddfed1ce33065c1ccf871053b844218cd3d4
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-03-13 14:04:25 -0500

    dialog structure

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,10 +1,26 @@
 'use strict';
 
+// Assigning tumbnail picurese
 const homes = [...document.querySelectorAll('.home')];
-
 homes.forEach((home, index) => {
   home.style.backgroundImage = `url("./assets/media/homes/${index}.jpg")`;
   home.addEventListener('click', () => {
     window.location.href = 'detail.html'      
   });
+});
+
+//Login dialog interaction
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("#login-btn");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+//closes by clicking ouside of the dialog box
+dialog.addEventListener('click', (event) => {
+  if (event.target === dialog) {
+    dialog.close();
+  }
 });
